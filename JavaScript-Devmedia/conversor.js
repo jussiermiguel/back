e38dadc2d468ab2b01bf9b52d0f6a3b87aa7115b
jussiermiguel <jussier.miguel08@gmail.com>
2023-09-29 23:04:07 -0300

    refatoração

---
+++ JavaScript-Devmedia/conversor.js
@@ -0,0 +1,6 @@
+function converterParaCelsius (fahrenheit){
+    let celsius = (fahrenheit - 32) / 1.8;
+    return celsius; 
+}
+
+export default converterParaCelsius;
---
+++ JavaScript-Devmedia/index.js
@@ -0,0 +1,5 @@
+import converterParaCelsius from "./conversor";
+
+let fahrenheit = 66;
+
+console.log(converterParaCelsius(fahrenheit));
