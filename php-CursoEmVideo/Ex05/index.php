d96489a3098aecac8add273e8fe6231ef559b208
jussiermiguel <jussier.miguel08@gmail.com>
2023-10-06 16:57:14 -0300

    strings e sequências de escape

---
+++ php-CursoEmVideo/Ex05/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <h1>Sequência de escape</h1>
+    <?php 
+    $nome = "Rodrigo";
+    $sobrenome = 'Nogueira';
+    echo "$nome \"Minotauro\" $sobrenome";
+    ?>
+</body>
+</html>
